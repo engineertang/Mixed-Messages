9e03e312b769d78282d0af3f9dc17f03c1048162
donald <donald.tangze@outlook.com>
2023-09-05 14:33:42 +0800

    submit forcast method

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-## 
+## Project Title
+[Backend Online Course](https://www.codecademy.com/paths/back-end-engineer-career-path/tracks/becp-22-portfolio-project-mixed-messages/modules/becp-22-mixed-messages/kanban_projects/mixed-messages) 
+## Description: 
+1.JavaScript
+## Features: 
+
+## How to use: 
+These code can be run in browser or Node.js Environment.
+
+## Technologies:
+JavaScript Core Knowledge based on a Java or other Backend Developer
+
+
+## License: 
--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 'use strict'
 
+let professionArray = ['Teacher', 'Boss', 'Astronaut', 'Engineer']
+
+let ageArray = [22, 33, 44, 55, 66]
+
+function forcast(){
+
+    let professionRandom = Math.floor(Math.random() * professionArray.length);
+    let ageRandom = Math.floor(Math.random() * ageArray.length);
+
+    let result = `You would be a ${professionArray.at(professionRandom)} when you are ${ageArray.at(ageRandom)} years old.`
+    return result
+}
+
+console.log(forcast())

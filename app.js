5684f42922ab2b23480e87ae590bd5e7f09c64af
donald <donald.tangze@outlook.com>
2023-09-05 14:46:14 +0800

    organise three array in an object

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 'use strict'
 
-let professionArray = ['Teacher', 'Boss', 'Astronaut', 'Engineer']
+let array = {
+    professionArray: ['Teacher', 'Boss', 'Astronaut', 'Engineer'],
+    ageArray: [22, 33, 44, 55, 66],
+    locationArray: ['Hong Kong', 'Shenzhen', 'Beihai', 'Silicon Valley']
+}
 
-let ageArray = [22, 33, 44, 55, 66]
-
-let locationArray = ['Hong Kong', 'Shenzhen', 'Beihai', 'Silicon Valley']
 
 function generateRandom(length){
     return Math.floor(Math.random() * length)
 }
 function forcast(){
 
-    let professionRandom = generateRandom (professionArray.length);
-    let ageRandom = generateRandom(ageArray.length);
-    let locationRandom = generateRandom(locationArray.length);
-    console.log(locationRandom)
+    let professionRandom = generateRandom (array.professionArray.length);
+    let ageRandom = generateRandom(array.ageArray.length);
+    let locationRandom = generateRandom(array.locationArray.length);
 
-    let result = `You would be a ${professionArray.at(professionRandom)} when you are ${ageArray.at(ageRandom)} years old, and You would live in ${locationArray[locationRandom]}`
+    let result = `You would be a ${array.professionArray.at(professionRandom)} when you are ${array.ageArray.at(ageRandom)} years old, and You would live in ${array.locationArray[locationRandom]}.`
     return result
 }
 

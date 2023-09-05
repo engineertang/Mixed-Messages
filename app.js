a54409897c3e57e26e93418055dc70ef65df0cd3
donald <donald.tangze@outlook.com>
2023-09-05 14:43:57 +0800

    abstract function generateRandom

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ let professionArray = ['Teacher', 'Boss', 'Astronaut', 'Engineer']
 
 let ageArray = [22, 33, 44, 55, 66]
 
+let locationArray = ['Hong Kong', 'Shenzhen', 'Beihai', 'Silicon Valley']
+
+function generateRandom(length){
+    return Math.floor(Math.random() * length)
+}
 function forcast(){
 
-    let professionRandom = Math.floor(Math.random() * professionArray.length);
-    let ageRandom = Math.floor(Math.random() * ageArray.length);
+    let professionRandom = generateRandom (professionArray.length);
+    let ageRandom = generateRandom(ageArray.length);
+    let locationRandom = generateRandom(locationArray.length);
+    console.log(locationRandom)
 
-    let result = `You would be a ${professionArray.at(professionRandom)} when you are ${ageArray.at(ageRandom)} years old.`
+    let result = `You would be a ${professionArray.at(professionRandom)} when you are ${ageArray.at(ageRandom)} years old, and You would live in ${locationArray[locationRandom]}`
     return result
 }
 
